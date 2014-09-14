20b3c2ac321f2ef3698baf546a4463708797ecf4
Eric Grounds <egroundsy@gmail.com>
2014-09-14 15:41:50 -0500

    Initial commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Eric on 9/14/14.
+//  Copyright (c) 2014 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
